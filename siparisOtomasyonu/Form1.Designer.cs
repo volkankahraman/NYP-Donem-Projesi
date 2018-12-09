11986fa6c2c1099f55325e38915be02f67697fd1
volkankahraman <khrmnvolkan@gmail.com>
2018-12-10 00:53:09 +0300

    temel

--- siparisOtomasyonu/Form1.Designer.cs
+++ siparisOtomasyonu/Form1.Designer.cs
@@ -28,32 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(222, 152);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Sipariş Otomasyonu";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Sipariş Otomasyonu";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            this.Text = "Form1";
         }
 
         #endregion
+<<<<<<< HEAD
 
         private System.Windows.Forms.Label label1;
 <<<<<<< HEAD
@@ -68,6 +50,8 @@
 >>>>>>> parent of 09e72e2... temel
 =======
 >>>>>>> parent of c956b7e... Button Ekledim
+=======
+>>>>>>> parent of 09e72e2... temel
     }
 }
 
