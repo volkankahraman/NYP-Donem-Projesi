f6074dd006a23985514cadcfeb31a4c48950a828
Ulas <Ulas@DESKTOP-H2C1B1Q>
2018-12-26 21:43:20 +0300

    payment edit

--- TermProject/TermProject/Forms/PaymentPanel.Designer.cs
+++ TermProject/TermProject/Forms/PaymentPanel.Designer.cs
@@ -32,30 +32,23 @@
             this.rbCheck = new System.Windows.Forms.RadioButton();
             this.rbCreditCard = new System.Windows.Forms.RadioButton();
             this.rbCash = new System.Windows.Forms.RadioButton();
-            this.gbCash = new System.Windows.Forms.GroupBox();
-            this.lblCashTendered = new System.Windows.Forms.Label();
-            this.txtCashTendered = new System.Windows.Forms.TextBox();
-            this.gbCheck = new System.Windows.Forms.GroupBox();
-            this.lblCheckName = new System.Windows.Forms.Label();
-            this.txtCheckNBR = new System.Windows.Forms.TextBox();
-            this.lblCheckNBR = new System.Windows.Forms.Label();
-            this.gbCreditCard = new System.Windows.Forms.GroupBox();
             this.cbCreditType = new System.Windows.Forms.ComboBox();
             this.lblCCNumber = new System.Windows.Forms.Label();
             this.lblExpDate = new System.Windows.Forms.Label();
             this.lblCCType = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblCashTendered = new System.Windows.Forms.Label();
+            this.txtCashTendered = new System.Windows.Forms.TextBox();
             this.txtExpDate = new System.Windows.Forms.TextBox();
             this.txtCCNumber = new System.Windows.Forms.TextBox();
+            this.lblCheckName = new System.Windows.Forms.Label();
+            this.lblCheckNBR = new System.Windows.Forms.Label();
             this.txtCheckName = new System.Windows.Forms.TextBox();
             this.btnPay = new System.Windows.Forms.Button();
             this.lblTotalTax = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
             this.lblTotalWeight = new System.Windows.Forms.Label();
+            this.txtCheckNBR = new System.Windows.Forms.TextBox();
             this.gbPaymentType.SuspendLayout();
-            this.gbCash.SuspendLayout();
-            this.gbCheck.SuspendLayout();
-            this.gbCreditCard.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbPaymentType
@@ -106,22 +99,52 @@
             this.rbCash.UseVisualStyleBackColor = true;
             this.rbCash.CheckedChanged += new System.EventHandler(this.rbCash_CheckedChanged);
             // 
-            // gbCash
+            // cbCreditType
             // 
-            this.gbCash.Controls.Add(this.lblCashTendered);
-            this.gbCash.Controls.Add(this.txtCashTendered);
-            this.gbCash.Location = new System.Drawing.Point(18, 175);
-            this.gbCash.Name = "gbCash";
-            this.gbCash.Size = new System.Drawing.Size(410, 155);
-            this.gbCash.TabIndex = 1;
-            this.gbCash.TabStop = false;
-            this.gbCash.Text = "Nakit";
-            this.gbCash.Visible = false;
+            this.cbCreditType.FormattingEnabled = true;
+            this.cbCreditType.Items.AddRange(new object[] {
+            "MasterCard",
+            "Visa"});
+            this.cbCreditType.Location = new System.Drawing.Point(158, 185);
+            this.cbCreditType.Name = "cbCreditType";
+            this.cbCreditType.Size = new System.Drawing.Size(121, 21);
+            this.cbCreditType.TabIndex = 13;
+            this.cbCreditType.Visible = false;
+            // 
+            // lblCCNumber
+            // 
+            this.lblCCNumber.AutoSize = true;
+            this.lblCCNumber.Location = new System.Drawing.Point(29, 169);
+            this.lblCCNumber.Name = "lblCCNumber";
+            this.lblCCNumber.Size = new System.Drawing.Size(99, 13);
+            this.lblCCNumber.TabIndex = 12;
+            this.lblCCNumber.Text = "Kredi kartı numarası";
+            this.lblCCNumber.Visible = false;
+            // 
+            // lblExpDate
+            // 
+            this.lblExpDate.AutoSize = true;
+            this.lblExpDate.Location = new System.Drawing.Point(285, 169);
+            this.lblExpDate.Name = "lblExpDate";
+            this.lblExpDate.Size = new System.Drawing.Size(96, 13);
+            this.lblExpDate.TabIndex = 7;
+            this.lblExpDate.Text = "Son kullanma tarihi";
+            this.lblExpDate.Visible = false;
+            // 
+            // lblCCType
+            // 
+            this.lblCCType.AutoSize = true;
+            this.lblCCType.Location = new System.Drawing.Point(155, 170);
+            this.lblCCType.Name = "lblCCType";
+            this.lblCCType.Size = new System.Drawing.Size(75, 13);
+            this.lblCCType.TabIndex = 9;
+            this.lblCCType.Text = "Kredi Kartı Tipi";
+            this.lblCCType.Visible = false;
             // 
             // lblCashTendered
             // 
             this.lblCashTendered.AutoSize = true;
-            this.lblCashTendered.Location = new System.Drawing.Point(17, 38);
+            this.lblCashTendered.Location = new System.Drawing.Point(30, 169);
             this.lblCashTendered.Name = "lblCashTendered";
             this.lblCashTendered.Size = new System.Drawing.Size(66, 13);
             this.lblCashTendered.TabIndex = 5;
@@ -129,134 +152,55 @@
             // 
             // txtCashTendered
             // 
-            this.txtCashTendered.Location = new System.Drawing.Point(20, 54);
+            this.txtCashTendered.Location = new System.Drawing.Point(32, 186);
             this.txtCashTendered.Name = "txtCashTendered";
             this.txtCashTendered.Size = new System.Drawing.Size(100, 20);
             this.txtCashTendered.TabIndex = 4;
+            
             // 
-            // gbCheck
+            // txtExpDate
             // 
-            this.gbCheck.Controls.Add(this.lblCheckName);
-            this.gbCheck.Controls.Add(this.txtCheckNBR);
-            this.gbCheck.Controls.Add(this.lblCheckNBR);
-            this.gbCheck.Controls.Add(this.gbCreditCard);
-            this.gbCheck.Controls.Add(this.txtCheckName);
-            this.gbCheck.Location = new System.Drawing.Point(18, 188);
-            this.gbCheck.Name = "gbCheck";
-            this.gbCheck.Size = new System.Drawing.Size(410, 136);
-            this.gbCheck.TabIndex = 3;
-            this.gbCheck.TabStop = false;
-            this.gbCheck.Text = "Çek";
-            this.gbCheck.Visible = false;
+            this.txtExpDate.Location = new System.Drawing.Point(288, 185);
+            this.txtExpDate.Name = "txtExpDate";
+            this.txtExpDate.Size = new System.Drawing.Size(100, 20);
+            this.txtExpDate.TabIndex = 2;
+            this.txtExpDate.Visible = false;
+            // 
+            // txtCCNumber
+            // 
+            this.txtCCNumber.Location = new System.Drawing.Point(32, 185);
+            this.txtCCNumber.Name = "txtCCNumber";
+            this.txtCCNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtCCNumber.TabIndex = 3;
+            this.txtCCNumber.Visible = false;
             // 
             // lblCheckName
             // 
             this.lblCheckName.AutoSize = true;
-            this.lblCheckName.Location = new System.Drawing.Point(17, 40);
+            this.lblCheckName.Location = new System.Drawing.Point(30, 169);
             this.lblCheckName.Name = "lblCheckName";
             this.lblCheckName.Size = new System.Drawing.Size(25, 13);
             this.lblCheckName.TabIndex = 13;
             this.lblCheckName.Text = "İsim";
-            // 
-            // txtCheckNBR
-            // 
-            this.txtCheckNBR.Location = new System.Drawing.Point(190, 56);
-            this.txtCheckNBR.Name = "txtCheckNBR";
-            this.txtCheckNBR.Size = new System.Drawing.Size(100, 20);
-            this.txtCheckNBR.TabIndex = 4;
+            this.lblCheckName.Visible = false;
             // 
             // lblCheckNBR
             // 
             this.lblCheckNBR.AutoSize = true;
-            this.lblCheckNBR.Location = new System.Drawing.Point(187, 40);
+            this.lblCheckNBR.Location = new System.Drawing.Point(155, 170);
             this.lblCheckNBR.Name = "lblCheckNBR";
             this.lblCheckNBR.Size = new System.Drawing.Size(71, 13);
             this.lblCheckNBR.TabIndex = 15;
             this.lblCheckNBR.Text = "Çek numarası";
-            // 
-            // gbCreditCard
-            // 
-            this.gbCreditCard.Controls.Add(this.cbCreditType);
-            this.gbCreditCard.Controls.Add(this.lblCCNumber);
-            this.gbCreditCard.Controls.Add(this.lblExpDate);
-            this.gbCreditCard.Controls.Add(this.lblCCType);
-            this.gbCreditCard.Controls.Add(this.label4);
-            this.gbCreditCard.Controls.Add(this.txtExpDate);
-            this.gbCreditCard.Controls.Add(this.txtCCNumber);
-            this.gbCreditCard.Location = new System.Drawing.Point(0, 0);
-            this.gbCreditCard.Name = "gbCreditCard";
-            this.gbCreditCard.Size = new System.Drawing.Size(410, 136);
-            this.gbCreditCard.TabIndex = 2;
-            this.gbCreditCard.TabStop = false;
-            this.gbCreditCard.Text = "Kredi kartı";
-            this.gbCreditCard.Visible = false;
-            // 
-            // cbCreditType
-            // 
-            this.cbCreditType.FormattingEnabled = true;
-            this.cbCreditType.Items.AddRange(new object[] {
-            "MasterCard",
-            "Visa"});
-            this.cbCreditType.Location = new System.Drawing.Point(146, 60);
-            this.cbCreditType.Name = "cbCreditType";
-            this.cbCreditType.Size = new System.Drawing.Size(121, 21);
-            this.cbCreditType.TabIndex = 13;
-            // 
-            // lblCCNumber
-            // 
-            this.lblCCNumber.AutoSize = true;
-            this.lblCCNumber.Location = new System.Drawing.Point(17, 44);
-            this.lblCCNumber.Name = "lblCCNumber";
-            this.lblCCNumber.Size = new System.Drawing.Size(99, 13);
-            this.lblCCNumber.TabIndex = 12;
-            this.lblCCNumber.Text = "Kredi kartı numarası";
-            // 
-            // lblExpDate
-            // 
-            this.lblExpDate.AutoSize = true;
-            this.lblExpDate.Location = new System.Drawing.Point(273, 44);
-            this.lblExpDate.Name = "lblExpDate";
-            this.lblExpDate.Size = new System.Drawing.Size(96, 13);
-            this.lblExpDate.TabIndex = 7;
-            this.lblExpDate.Text = "Son kullanma tarihi";
-            // 
-            // lblCCType
-            // 
-            this.lblCCType.AutoSize = true;
-            this.lblCCType.Location = new System.Drawing.Point(143, 44);
-            this.lblCCType.Name = "lblCCType";
-            this.lblCCType.Size = new System.Drawing.Size(75, 13);
-            this.lblCCType.TabIndex = 9;
-            this.lblCCType.Text = "Kredi Kartı Tipi";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 44);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 13);
-            this.label4.TabIndex = 11;
-            // 
-            // txtExpDate
-            // 
-            this.txtExpDate.Location = new System.Drawing.Point(276, 60);
-            this.txtExpDate.Name = "txtExpDate";
-            this.txtExpDate.Size = new System.Drawing.Size(100, 20);
-            this.txtExpDate.TabIndex = 2;
-            // 
-            // txtCCNumber
-            // 
-            this.txtCCNumber.Location = new System.Drawing.Point(20, 60);
-            this.txtCCNumber.Name = "txtCCNumber";
-            this.txtCCNumber.Size = new System.Drawing.Size(100, 20);
-            this.txtCCNumber.TabIndex = 3;
+            this.lblCheckNBR.Visible = false;
             // 
             // txtCheckName
             // 
-            this.txtCheckName.Location = new System.Drawing.Point(20, 56);
+            this.txtCheckName.Location = new System.Drawing.Point(32, 186);
             this.txtCheckName.Name = "txtCheckName";
             this.txtCheckName.Size = new System.Drawing.Size(100, 20);
             this.txtCheckName.TabIndex = 0;
+            this.txtCheckName.Visible = false;
             // 
             // btnPay
             // 
@@ -296,29 +240,41 @@
             this.lblTotalWeight.TabIndex = 16;
             this.lblTotalWeight.Text = "Toplam ağırlık:";
             // 
+            // txtCheckNBR
+            // 
+            this.txtCheckNBR.Location = new System.Drawing.Point(158, 186);
+            this.txtCheckNBR.Name = "txtCheckNBR";
+            this.txtCheckNBR.Size = new System.Drawing.Size(100, 20);
+            this.txtCheckNBR.TabIndex = 4;
+            this.txtCheckNBR.Visible = false;
+            // 
             // Payment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 456);
+            this.Controls.Add(this.cbCreditType);
+            this.Controls.Add(this.lblCashTendered);
+            this.Controls.Add(this.lblCCNumber);
+            this.Controls.Add(this.lblExpDate);
+            this.Controls.Add(this.txtCheckNBR);
+            this.Controls.Add(this.lblCCType);
+            this.Controls.Add(this.txtCashTendered);
+            this.Controls.Add(this.lblCheckName);
+            this.Controls.Add(this.txtExpDate);
+            this.Controls.Add(this.lblCheckNBR);
+            this.Controls.Add(this.txtCCNumber);
+            this.Controls.Add(this.txtCheckName);
             this.Controls.Add(this.lblTotalWeight);
-            this.Controls.Add(this.gbCheck);
             this.Controls.Add(this.lblTotal);
             this.Controls.Add(this.lblTotalTax);
             this.Controls.Add(this.btnPay);
-            this.Controls.Add(this.gbCash);
             this.Controls.Add(this.gbPaymentType);
             this.Name = "Payment";
             this.Text = "Payment";
             this.Load += new System.EventHandler(this.Payment_Load);
             this.gbPaymentType.ResumeLayout(false);
             this.gbPaymentType.PerformLayout();
-            this.gbCash.ResumeLayout(false);
-            this.gbCash.PerformLayout();
-            this.gbCheck.ResumeLayout(false);
-            this.gbCheck.PerformLayout();
-            this.gbCreditCard.ResumeLayout(false);
-            this.gbCreditCard.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -330,9 +286,6 @@
         private System.Windows.Forms.RadioButton rbCheck;
         private System.Windows.Forms.RadioButton rbCreditCard;
         private System.Windows.Forms.RadioButton rbCash;
-        private System.Windows.Forms.GroupBox gbCash;
-        private System.Windows.Forms.GroupBox gbCreditCard;
-        private System.Windows.Forms.GroupBox gbCheck;
         private System.Windows.Forms.TextBox txtExpDate;
         private System.Windows.Forms.TextBox txtCCNumber;
         private System.Windows.Forms.TextBox txtCheckName;
@@ -341,14 +294,13 @@
         private System.Windows.Forms.TextBox txtCashTendered;
         private System.Windows.Forms.Label lblExpDate;
         private System.Windows.Forms.Label lblCCType;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblCheckName;
         private System.Windows.Forms.Label lblCheckNBR;
-        private System.Windows.Forms.TextBox txtCheckNBR;
         private System.Windows.Forms.ComboBox cbCreditType;
         private System.Windows.Forms.Label lblCCNumber;
         private System.Windows.Forms.Label lblTotalTax;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Label lblTotalWeight;
+        private System.Windows.Forms.TextBox txtCheckNBR;
     }
 }
--- TermProject/TermProject/Forms/PaymentPanel.cs
+++ TermProject/TermProject/Forms/PaymentPanel.cs
@@ -11,15 +11,13 @@ using System.Windows.Forms;
 using TermProject.Models.PaymentModels;
 using TermProject.Models.OrderModels;
 using TermProject.DataSource;
+using TermProject.Models;
 
 namespace TermProject
 {
     public partial class Payment : Form
     {
-        private void Change()
-        {
-            MessageBox.Show("Para Üstünüz:" + (Convert.ToInt32(txtCashTendered.Text) - Convert.ToInt32(DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTotal())));
-        }
+
         public Payment()
         {
             InitializeComponent();
@@ -27,15 +25,43 @@ namespace TermProject
         }
         Order order;
         Models.PaymentModels.Payment payment;
+        Customer ActiveCustomer = DataSourceSingleton.GetInstance().ActiveCustomer;
+        void ClearTextBox()
+        {
+
+
+        }
         private void btnPay_Click(object sender, EventArgs e)
         {
+            string _CashTendered = txtCashTendered.Text;
+            int CashTendered;
+
             if (rbCash.Checked == true)
             {
-                Cash cash = new Cash();
-                cash.CashTendered = txtCashTendered.Text;
-                cash.Amount = Convert.ToInt32(DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTotal());
-                payment = cash;
-                Change();
+                if (int.TryParse(_CashTendered, out CashTendered) == true)
+                {
+                    if (CashTendered - Convert.ToInt32(ActiveCustomer.CalculateTotal()) >= 0)
+                    {
+                        Cash cash = new Cash();
+                        cash.CashTendered = txtCashTendered.Text;
+                        cash.Amount = Convert.ToInt32(ActiveCustomer.CalculateTotal());
+                        payment = cash;
+                        MessageBox.Show("Para Üstünüz:" + (Convert.ToInt32(txtCashTendered.Text) - Convert.ToInt32(ActiveCustomer.CalculateTotal())));
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Yetersiz tutar girdiniz");
+                        txtCashTendered.Text = string.Empty;
+                        txtCashTendered.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen sayı giriniz");
+                    txtCashTendered.Text = string.Empty;
+                    txtCashTendered.Focus();
+                }
             }
             if (rbCheck.Checked == true)
             {
@@ -44,7 +70,7 @@ namespace TermProject
                 check.Name = txtCheckName.Text;
                 check.BankID = txtCheckNBR.Text;
                 CheckAuthorize = check.Authorized();
-                check.Amount = Convert.ToInt32(DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTotal());
+                check.Amount = Convert.ToInt32(ActiveCustomer.CalculateTotal());
                 payment = check;
             }
             if (rbCreditCard.Checked == true)
@@ -56,53 +82,92 @@ namespace TermProject
                 credit.Type = (CreditType)(a);
                 credit.Number = txtCCNumber.Text;
                 CreditCardAuthorize = credit.Authorized();
-                credit.Amount = Convert.ToInt32(DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTotal());
+                credit.Amount = Convert.ToInt32(ActiveCustomer.CalculateTotal());
                 payment = credit;
             }
             order.Payment = payment;
-            for (int a = 0; a < DataSourceSingleton.GetInstance().ActiveCustomer.Cart.Count; a++)
+            for (int a = 0; a < ActiveCustomer.Cart.Count; a++)
             {
                 order.OrderDetails.Add(new OrderDetail()
                 {
-                    Item = DataSourceSingleton.GetInstance().ActiveCustomer.Cart[a].Item,
-                    Quantity = DataSourceSingleton.GetInstance().ActiveCustomer.Cart[a].Quantity,
-                    TaxStatus = DataSourceSingleton.GetInstance().ActiveCustomer.Cart[a].TaxStatus
+                    Item = ActiveCustomer.Cart[a].Item,
+                    Quantity = ActiveCustomer.Cart[a].Quantity,
+                    TaxStatus = ActiveCustomer.Cart[a].TaxStatus
                 });
             }
-            DataSourceSingleton.GetInstance().ActiveCustomer.Cart.Clear();
-            this.Close();
+            ActiveCustomer.Cart.Clear();
         }
         private void Payment_Load(object sender, EventArgs e)
         {
             order = new Order(DataSourceSingleton.GetInstance().OrderDetails);
             order.Date = DateTime.Now;
-            order.Customer = DataSourceSingleton.GetInstance().ActiveCustomer;
+            order.Customer = ActiveCustomer;
             DataSourceSingleton.GetInstance().OrderList.Add(order);
-            lblTotal.Text = "Toplam tutar:" + DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTotal();
-            lblTotalWeight.Text = "Toplam ağırlık:" + DataSourceSingleton.GetInstance().ActiveCustomer.CalculateWeight();
-            lblTotalTax.Text = "Toplam Vergi:" + DataSourceSingleton.GetInstance().ActiveCustomer.CalculateTax();
-        }
-        private void rbCash_CheckedChanged(object sender, EventArgs e)
-        {
-            gbCash.Visible = true;
-            btnPay.Visible = true;
-            gbCheck.Visible = false;
-            gbCreditCard.Visible = false;
+            lblTotal.Text = "Toplam tutar:" + ActiveCustomer.CalculateTotal();
+            lblTotalWeight.Text = "Toplam ağırlık:" + ActiveCustomer.CalculateWeight();
+            lblTotalTax.Text = "Toplam Vergi:" + ActiveCustomer.CalculateTax();
         }
         private void rbCreditCard_CheckedChanged(object sender, EventArgs e)
         {
-            gbCash.Visible = false;
             btnPay.Visible = true;
-            gbCheck.Visible = false;
-            gbCreditCard.Visible = true;
+
+            lblCCNumber.Visible = true;
+            lblCCType.Visible = true;
+            lblExpDate.Visible = true;
+            txtCCNumber.Visible = true;
+            cbCreditType.Visible = true;
+            txtExpDate.Visible = true;
+
+
+            txtCheckName.Visible = false;
+            txtCheckNBR.Visible = false;
+            lblCheckName.Visible = false;
+            lblCheckNBR.Visible = false;
+
+            lblCashTendered.Visible = false;
+            txtCashTendered.Visible = false;
         }
         private void rbCheck_CheckedChanged(object sender, EventArgs e)
         {
-
-            gbCash.Visible = false;
             btnPay.Visible = true;
-            gbCheck.Visible = true;
-            gbCreditCard.Visible = false;
+
+            txtCheckNBR.Visible = true;
+            lblCheckName.Visible = true;
+            lblCheckNBR.Visible = true;
+            txtCheckName.Visible = true;
+
+            lblCCNumber.Visible = false;
+            lblCCType.Visible = false;
+            lblExpDate.Visible = false;
+            txtCCNumber.Visible = false;
+            cbCreditType.Visible = false;
+            txtExpDate.Visible = false;
+
+            lblCashTendered.Visible = false;
+            txtCashTendered.Visible = false;
+
         }
+        private void rbCash_CheckedChanged(object sender, EventArgs e)
+        {
+            lblCashTendered.Visible = true;
+            txtCashTendered.Visible = true;
+            btnPay.Visible = true;
+
+            txtCheckName.Visible = false;
+            txtCheckNBR.Visible = false;
+            lblCheckName.Visible = false;
+            lblCheckNBR.Visible = false;
+
+
+            lblCCNumber.Visible = false;
+            lblCCType.Visible = false;
+            lblExpDate.Visible = false;
+            txtCCNumber.Visible = false;
+            cbCreditType.Visible = false;
+            txtExpDate.Visible = false;
+
+        }
+
+
     }
 }
